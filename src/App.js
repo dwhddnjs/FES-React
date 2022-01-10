b2dd939b1d78d52928b2d025c8b42e8230727677
dwhddnjs <per1215@naver.com>
2022-01-11 01:56:44 +0900

    distructed props

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import styled from "styled-components";
 
 const CardDiv = styled.div`
   padding: 20px;
-  border-radius: 10px;
+  border-radius: 20px;
   border: 1px solid #c4c4c4;
   margin-bottom: 20px;
   width: ${(props) => (props.className === "setting" ? "200px" : "400px")};
 `;
 
-const Card = (props) => {
+const Card = ({ value, setting, children }) => {
   return (
-    <CardDiv className={props.className}>
-      <h3>{props.value}</h3>
+    <CardDiv className={setting}>
+      <h3>{value}</h3>
       <hr />
-      <div>{props.children}</div>
+      <div>{children}</div>
     </CardDiv>
   );
 };
